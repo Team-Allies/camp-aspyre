3124fbb4cb5f387ce3e351b7783e07302f733f1a
danielwalker <dawalker2017@outlook.com>
2019-11-18 11:18:01 -0500

    Created URL for base.html for visual testing

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from core import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.base),
 ]
 
 if settings.DEBUG:
