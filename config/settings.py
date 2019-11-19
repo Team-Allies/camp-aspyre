3fa8ad7dfae9c0c1f7b4a59b2cb129cf9c0b70ff
Lisa Money <lisamoneylama@gmail.com>
2019-11-19 13:56:47 -0500

    getting heroku working

--- config/settings.py
+++ config/settings.py
@@ -87,10 +87,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aspyre',
+        'USER': 'aspyre’,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
