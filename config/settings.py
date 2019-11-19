eb0adbc112dcaf5e54f6867f14406b25686b897f
Lisa Money <lisamoneylama@gmail.com>
2019-11-19 13:08:13 -0500

    setting up heroku to work

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tac%fxds*4km*xp168c*y)$^r*73z=2y26@$2_o2$9x2kl()@b'
+SECRET_KEY = os.getenv('SECRET_KEY', 'tac%fxds*4km*xp168c*y)$^r*73z=2y26@$2_o2$9x2kl()@b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
