f35b43169a7a2fac878c0fe0e33d1d7f560708bc
jjporter <jjporter921@gmail.com>
2019-11-21 14:18:05 -0500

    creates view for camper registration form

--- core/views.py
+++ core/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from core.forms import CamperRegistrationForm
+from core.models import User, Camper
 
 def camper_registration(request):
-    return render(request, 'core/camper_registration.html')
+  form = CamperRegistrationForm(request.POST)
+  if request.method == 'POST':
+    if form.is_valid():
+      form.save()
+      pass
+      #redirect to new URL...probably payment page or scholarship form
+    else:
+      form = CamperRegistrationForm()
+  return render(request, 'core/camper_registration.html', {'form': form})
+
+  
