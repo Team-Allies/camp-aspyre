9df50577ac36652756d641187e29c170a8b41450
danielwalker <dawalker2017@outlook.com>
2019-11-22 11:22:11 -0500

    WIP - Created a view for the Camper medical form Model in order to pull items from the model into the html

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from core.forms import CamperRegistrationForm
-from core.models import User, Camper
+from core.models import User, Camper, Camp, MedicalInformation
+
+def index(request):
+  return render(request, 'core/index.html')
 
 def camper_registration(request):
   form = CamperRegistrationForm(request.POST)
@@ -13,4 +16,16 @@ def camper_registration(request):
       form = CamperRegistrationForm()
   return render(request, 'core/camper_registration.html', {'form': form})
 
+def camper_registration_submitted(request):
+  return render(request, 'core/camper_registration_submitted.html')
+
+def camper_medical_form(request):
+#   form = CamperMedicalForm(request.POST)
+#   if request.method == 'POST':
+#     if form.is_valid():
+#       form.save()
+#       pass
+#     else:
+#       form = CamperMedicalForm()
+  return render(request, 'core/camper_medical_form.html', {'form': form})
   
