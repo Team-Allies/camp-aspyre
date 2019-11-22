1d9f6185891606f64f15417ce3eae6f2f77bba62
danielwalker <dawalker2017@outlook.com>
2019-11-22 11:12:24 -0500

    Created admin interface entities that allow for tabs within each model

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,47 @@
 from django.contrib import admin
-from .models import User, Camper, Camp, Registration, Role
+from django.contrib.auth.admin import UserAdmin
+from core.models import User, Camper, Camp, Registration, Role, MedicalInformation
 
-# Register your models here.
+class CamperAdmin(admin.ModelAdmin):
+    fields = (
+        'legal_full_name',
+        'preferred_name',
+        'street_address',
+        'city',
+        'state',
+        'zip_code',
+        'previously_attended',
+        'user',
+    )
 
-admin.site.register(User)
-admin.site.register(Camper)
-admin.site.register(Camp)
+    list_display = (
+        'legal_full_name',
+        'preferred_name',
+        'street_address',
+        'city',
+        'state',
+        'zip_code',
+        'previously_attended',
+        'user',
+    )
+
+class CampAdmin(admin.ModelAdmin):
+    list_display = (
+    'name_of_camp',
+    'start_date',
+    'end_date',
+    'city_location',
+    'number_of_campers',
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Camper, CamperAdmin)
+admin.site.register(Camp, CampAdmin)
 admin.site.register(Registration)
 admin.site.register(Role)
+admin.site.register(MedicalInformation)
+
+
 
 
