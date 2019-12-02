87adaf5979337dbe193380013511620cb762c739
danielwalker <dawalker2017@outlook.com>
2019-12-02 09:26:45 -0500

    Changes to CSS, WIP to divide sections into a different page

--- config/urls.py
+++ config/urls.py
@@ -30,6 +30,18 @@ urlpatterns = [
     path('ASPYRE-Scholarship-submitted/', views.camper_scholarship_submitted, name="camper_scholarship_form_submitted"),
     path('ASPYRE-Medical-Form/', views.camper_medical_form, name="camper_medical_form"),
     path('ASPYRE-MedicalForm-submitted', views.camper_medical_form_submitted, name="camper_medical_form_submitted"),
+    path('medical_form_section_1', views.medical_form_section_1, name="medical_form_section_1"),
+    path('medical_form_section_2', views.medical_form_section_2, name="medical_form_section_2"),
+    path('medical_form_section_3', views.medical_form_section_3, name="medical_form_section_3"),
+    path('medical_form_section_4', views.medical_form_section_4, name="medical_form_section_4"),
+    path('medical_form_section_5', views.medical_form_section_5, name="medical_form_section_5"),
+    path('medical_form_section_6', views.medical_form_section_6, name="medical_form_section_6"),
+    path('medical_form_section_7', views.medical_form_section_7, name="medical_form_section_7"),
+    path('medical_form_section_8', views.medical_form_section_8, name="medical_form_section_8"),
+    path('medical_form_section_9', views.medical_form_section_9, name="medical_form_section_9"),
+    path('medical_form_section_10', views.medical_form_section_10, name="medical_form_section_10"),
+    path('medical_form_section_11', views.medical_form_section_11, name="medical_form_section_11"),
+
 ]
 
 if settings.DEBUG:
